f8ce82ff6604ae1dff1562a1e93f03b452c2e812
alexVillasenor <alejandro.villasenor97@gmail.com>
2018-10-31 14:31:31 -0600

    se conecta al servidor /home/

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {HomePage} from "../home/home";
+import {Http} from "@angular/http";
 
 /**
  * Generated class for the LoginPage page.
@@ -17,8 +18,10 @@ import {HomePage} from "../home/home";
 export class LoginPage {
 
   home = HomePage;
+  usuario = '';
+  password = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
   }
 
   ionViewDidLoad() {
@@ -27,10 +30,13 @@ export class LoginPage {
 
   clickHome(){
     //console.log("clickHome");
+    console.log(this.usuario);
+    console.log(this.password);
+
+    this.http.get("/home/").subscribe( data => {console.log(data.text())}, error1 => {console.log(".:Error:.")} )
 
     //this.navCtrl.push(this.home);
-
-    this.navCtrl.setRoot(this.home);
+    //this.navCtrl.setRoot(this.home);
   }
 
 }
