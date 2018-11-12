f091560fcb64f0265284a151650750a22b84d886
alexVillasenor <alejandro.villasenor97@gmail.com>
2018-11-12 14:33:46 -0600

    conexion api invernadero, proxy, recupera datos de base de datos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
+import {Http} from "@angular/http";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   invernaderos = [];
 
-  constructor(public navCtrl: NavController, public navParms: NavParams)
+  constructor(public navCtrl: NavController, public navParms: NavParams, public http: Http)
   {
-    console.log(this.navParms);
+    console.log(this.navParms.get('usuario'));
+
+    this.getInvernaderos();
   }
+
+  getInvernaderos(){
+    this.http.get('/invernadero/?usuario=' +
+      this.navParms.get('usuario'))
+      .subscribe(data => {
+        this.invernaderos=data.json();
+        console.log(data.text());
+      }, error1 => {
+        console.log('error');
+      });
+  }
+
 
 }
