930f7602e12e0fead9fe3b11a715abd01e17258c
alexVillasenor <alejandro.villasenor97@gmail.com>
2018-11-12 13:42:41 -0600

    12/11/18 login listo

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController, IonicPage, NavController, NavParams } from 'ionic-angular';
 import {HomePage} from "../home/home";
 import {Http} from "@angular/http";
 
@@ -21,7 +21,7 @@ export class LoginPage {
   usuario = '';
   password = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
@@ -33,7 +33,38 @@ export class LoginPage {
     console.log(this.usuario);
     console.log(this.password);
 
-    this.http.get("/home/").subscribe( data => {console.log(data.text())}, error1 => {console.log(".:Error:.")} )
+    this.http.get("/login/?usuario=" + this.usuario + "&password="+this.password)
+      .subscribe( data => {
+        console.log(data.text());
+    if(data.text() == "True"){
+      //mandar a la siguietne pagina
+      this.navCtrl.setRoot(this.home, {usuario: this.usuario});
+    }
+    else{
+      //mandar alerta
+      const alerta = this.alertCtrl.create(
+        {
+          title: 'Oops!',
+          subTitle: 'Usuario/Contraseña incorrectos',
+          buttons: ['Ok']
+        }
+      );
+      alerta.present();
+    }
+  },
+  error =>{console.log("error");
+
+    const alerta = this.alertCtrl.create(
+      {
+        title: 'Error',
+        subTitle: 'Error en la conexion',
+        buttons: ['Ok']
+      }
+    );
+    alerta.present();
+      }
+      );
+
 
     //this.navCtrl.push(this.home);
     //this.navCtrl.setRoot(this.home);
